a987af2292a22ec13c4c504178f4a1ecd56430db
Timo Uhlmann <timouhlmann@gmail.com>
2017-08-11 21:18:39 -0400

    Extract hero images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,6 +4,9 @@ import React from 'react'
 import connect, { ClientOnly } from 'webpack-react-compiler-plugin/runtime'
 
 import Layout from './layouts'
+import { loadImages } from './components/util/Image'
+
+const images = require.context('../images/2017-08-02', false, /\.(?:jpe?g|png)$/i)
 
 export default class Index extends React.Component {
   render () {
@@ -25,7 +28,8 @@ export default class Index extends React.Component {
 
 Index.layoutProps = {
   layout: Layout,
-  title: 'Home'
+  title: 'Home',
+  heroImages: loadImages(images)
 }
 
 connect(exports)
--- src/scripts/layouts/index.js
+++ src/scripts/layouts/index.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 import Header from '../components/header/Header'
-import { loadImages } from '../components/util/Image'
 
-const images = require.context('../../images/2017-08-02', false, /\.(?:jpe?g|png)$/i)
+const TITLE = 'Timo Goes To Canada'
 
 export default (props) => {
   const {
     children,
-    title
+    title,
+    heroImages
   } = props
   return (
     <html>
@@ -17,11 +17,11 @@ export default (props) => {
         <meta httpEquiv='x-ua-compatible' content='ie=edge' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='stylesheet' href='/styles.css' />
-        <title>{`Blog | ${title}`}</title>
+        <title>{`${title} – ${TITLE}` || TITLE}</title>
       </head>
       <body className='o-layout'>
         <Header className='o-layout__header'>
-          {loadImages(images)}
+          {heroImages}
         </Header>
         <main className='o-layout__content'>
           {children}
