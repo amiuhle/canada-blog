7e4346818f0ec18d7b9a0aafa76184c603a91b29
Timo Uhlmann <timouhlmann@gmail.com>
2017-08-21 00:22:53 -0400

    List recent posts on home page

--- src/scripts/pages/home.js
+++ src/scripts/pages/home.js
@@ -1,16 +1,33 @@
 import React from 'react'
 
 import Layout from '../layouts'
+
+import { getPosts } from '../util/posts'
+
 import { loadImages } from '../components/util/Image'
+import PostPreview from '../components/posts/PostPreview'
 
 const images = require.context('../../images/2017-08-02', false, /\.(?:jpe?g|png)$/i)
 
 export default class Home extends React.Component {
   render () {
+    const posts = getPosts(3)
     return (
       <div>
-        <h2>Welcome</h2>
-        <p />
+        <h2>Hi there!</h2>
+        <p>
+          Welcome to my Work &amp; Travel in Canada blog!
+        </p>
+        <p>
+          Posts are in English or German, depending on the main target audience.<br />
+          You can read more <a href='/about.html'>about me here</a>.
+        </p>
+
+        <h2>Recent Posts</h2>
+        {
+          posts.map((post, key) =>
+            <PostPreview className='o-post-list__item' key={key} post={post} />)
+        }
       </div>
     )
   }
